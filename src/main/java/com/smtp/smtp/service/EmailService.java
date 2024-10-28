81fe4b394153b9c5d172e90e0ea128b874e61184
João Vitor Braz <jvtomedi@gmail.com>
2024-10-27 21:48:08 -0300

    message.setFrom pulling from providerName

--- src/main/java/com/smtp/smtp/service/EmailService.java
+++ src/main/java/com/smtp/smtp/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.smtp.smtp.service;
 
+import com.smtp.smtp.entity.SmtpProvider;
+import com.smtp.smtp.repository.SmtpProviderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -14,13 +16,17 @@ public class EmailService {
     @Autowired
     private SmtpService smtpService;
 
-    public void sendSimpleEmail(String to, String subject, String text, Long id) {
+    @Autowired
+    private SmtpProviderRepository smtpProviderRepository;
+
+    public void sendSimpleEmail(String to, String subject, String html, Long id) {
         smtpService.setSmtpConfig(id);
+        SmtpProvider smtpProvider = smtpProviderRepository.findById(id).orElseThrow();
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
-        message.setText(text);
-        message.setFrom("EMAIL");
+        message.setText(html);
+        message.setFrom(smtpProvider.getProviderName());
 
         mailSender.send(message);
     }
